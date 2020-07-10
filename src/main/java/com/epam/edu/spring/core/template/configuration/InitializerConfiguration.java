d0f74b6ada94549a216dd5500a86496fe2ccb505
Pavel Antropov <p.m.antropov@gmail.com>
2020-07-10 14:52:54 +0400

    Added beans to configurations

--- src/main/java/com/epam/edu/spring/core/template/configuration/InitializerConfiguration.java
+++ src/main/java/com/epam/edu/spring/core/template/configuration/InitializerConfiguration.java
@@ -1,4 +1,14 @@
 package com.epam.edu.spring.core.template.configuration;
 
+import com.epam.edu.spring.core.template.factory.ColorFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class InitializerConfiguration {
+
+    @Bean
+    public ColorFactory colorFactory() {
+        return new ColorFactory();
+    }
 }
--- src/main/java/com/epam/edu/spring/core/template/configuration/RepositoryConfiguration.java
+++ src/main/java/com/epam/edu/spring/core/template/configuration/RepositoryConfiguration.java
@@ -1,4 +1,20 @@
 package com.epam.edu.spring.core.template.configuration;
 
+import com.epam.edu.spring.core.template.repository.ArrayListItemRepository;
+import com.epam.edu.spring.core.template.repository.LinkedListItemRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RepositoryConfiguration {
+
+    @Bean
+    public ArrayListItemRepository arrayListItemRepository() {
+        return new ArrayListItemRepository();
+    }
+
+    @Bean
+    public LinkedListItemRepository linkedListItemRepository() {
+        return new LinkedListItemRepository();
+    }
 }
