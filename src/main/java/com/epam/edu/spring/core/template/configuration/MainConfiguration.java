bd623ce4fa224df29daaf0d6f91a0fa00cb4c1ff
Pavel Antropov <p.m.antropov@gmail.com>
2020-07-12 12:08:18 +0400

    Imported configuration classes to MainConfiguration

--- src/main/java/com/epam/edu/spring/core/template/configuration/MainConfiguration.java
+++ src/main/java/com/epam/edu/spring/core/template/configuration/MainConfiguration.java
@@ -1,7 +1,9 @@
 package com.epam.edu.spring.core.template.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import({InitializerConfiguration.class, RepositoryConfiguration.class})
 public class MainConfiguration {
 }
