a446d3bbc084076e0a89bfad5986af9b0bdc5d46
Pavel Antropov <p.m.antropov@gmail.com>
2020-07-14 22:50:52 +0400

    Added some bean annotations

--- src/main/java/com/epam/edu/spring/core/template/validator/SimpleItemValidator.java
+++ src/main/java/com/epam/edu/spring/core/template/validator/SimpleItemValidator.java
@@ -1,7 +1,9 @@
 package com.epam.edu.spring.core.template.validator;
 
 import com.epam.edu.spring.core.template.entity.Item;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SimpleItemValidator implements ItemValidator {
 
     @Override
