9f58eec3350a2d91926b0cb4c33a7b8d138d727b
Pavel Antropov <p.m.antropov@gmail.com>
2020-07-14 22:52:46 +0400

    Fixed typo in SimpleItemService - changed ItemService field to ItemRepository

--- src/main/java/com/epam/edu/spring/core/template/service/SimpleItemService.java
+++ src/main/java/com/epam/edu/spring/core/template/service/SimpleItemService.java
@@ -1,19 +1,21 @@
 package com.epam.edu.spring.core.template.service;
 
 import com.epam.edu.spring.core.template.entity.Item;
+import com.epam.edu.spring.core.template.repository.ItemRepository;
 import com.epam.edu.spring.core.template.validator.ItemValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SimpleItemService implements ItemService {
 
-    private ItemService itemService;
+    private ItemRepository itemRepository;
     private ItemValidator itemValidator;
 
     @Autowired
-    SimpleItemService(ItemService itemService, ItemValidator itemValidator) {
-        this.itemService = itemService;
+    SimpleItemService(@Qualifier("array") ItemRepository itemRepository, ItemValidator itemValidator) {
+        this.itemRepository = itemRepository;
         this.itemValidator = itemValidator;
     }
 
@@ -27,9 +29,19 @@ public class SimpleItemService implements ItemService {
         return false;
     }
 
+    public ItemRepository getItemRepository() {
+        return this.itemRepository;
+    }
+
     @Autowired
-    public void setItemService(ItemService itemService) {
-        this.itemService = itemService;
+    // TODO:
+    @Qualifier("${item.repository.implementation}")
+    public void setItemRepository(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    public ItemValidator getItemValidator() {
+        return this.itemValidator;
     }
 
     @Autowired
